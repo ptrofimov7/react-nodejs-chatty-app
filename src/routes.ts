2cb38dbbcb546dbf10e99204a9e244a06dd1a42c
Peter Trofimov <ptrofimov7@gmail.com>
2023-08-19 16:17:13 +0300

    add comments-feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { authRoutes } from '@auth/routes/authRoutes';
 import { currentUserRoutes } from '@auth/routes/currentRoutes';
+import { commentRoutes } from '@comment/routes/commentRoutes';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { imageRoutes } from '@image/routes/imageRoutes';
 import { postRoutes } from '@post/routes/postRoutes';
@@ -24,7 +25,7 @@ export default (app: Application) => {
 
     app.use(BASE_PATH, authMiddleware.verifyUser, postRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, reactionRoutes.routes());
-    // app.use(BASE_PATH, authMiddleware.verifyUser, commentRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, commentRoutes.routes());
     // app.use(BASE_PATH, authMiddleware.verifyUser, followerRoutes.routes());
     // app.use(BASE_PATH, authMiddleware.verifyUser, notificationRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, imageRoutes.routes());
