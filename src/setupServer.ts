8a7aec52e0e0a2e7f1db47054f6c5592ccfe80a8
Peter Trofimov <ptrofimov7@gmail.com>
2023-08-21 17:00:49 +0300

    created user seeds and scripts

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,4 +1,3 @@
-import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
 import { Application, json, urlencoded, Response, Request, NextFunction } from 'express';
 import http from 'http';
 import cors from 'cors';
@@ -6,14 +5,16 @@ import helmet from 'helmet';
 import hpp from 'hpp';
 import compression from 'compression';
 import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
 import { Server } from 'socket.io';
 import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
-import HTTP_STATUS from 'http-status-codes';
+import Logger from 'bunyan';
+import apiStats from 'swagger-stats';
 import 'express-async-errors';
 import { config } from '@root/config';
 import applicationRoutes from '@root/routes';
-import Logger from 'bunyan';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
 import { SocketIOPostHandler } from '@socket/post';
 import { SocketIOFollowerHandler } from '@socket/follower';
 import { SocketIOUserHandler } from '@socket/user';
@@ -23,6 +24,7 @@ import { SocketIOChatHandler } from '@socket/chat';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
+
 export class ChattyServer {
   private app: Application;
 
@@ -33,18 +35,21 @@ export class ChattyServer {
   public start(): void {
     this.securityMiddleware(this.app);
     this.standardMiddleware(this.app);
-    this.routeMiddleware(this.app);
-    this.globalErrorHnadler(this.app);
+    this.routesMiddleware(this.app);
+    this.apiMonitoring(this.app);
+    this.globalErrorHandler(this.app);
     this.startServer(this.app);
   }
 
   private securityMiddleware(app: Application): void {
+    app.set('trust proxy', 1);
     app.use(
       cookieSession({
         name: 'session',
         keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
         maxAge: 24 * 7 * 3600000,
-        secure: config.NODE_ENV !== 'development'
+        secure: config.NODE_ENV !== 'development',
+        sameSite: 'none' // comment this line when running the server locally
       })
     );
     app.use(hpp());
@@ -65,11 +70,19 @@ export class ChattyServer {
     app.use(urlencoded({ extended: true, limit: '50mb' }));
   }
 
-  private routeMiddleware(app: Application): void {
+  private routesMiddleware(app: Application): void {
     applicationRoutes(app);
   }
 
-  private globalErrorHnadler(app: Application): void {
+  private apiMonitoring(app: Application): void {
+    app.use(
+      apiStats.getMiddleware({
+        uriPath: '/api-monitoring'
+      })
+    );
+  }
+
+  private globalErrorHandler(app: Application): void {
     app.all('*', (req: Request, res: Response) => {
       res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
     });
@@ -84,11 +97,14 @@ export class ChattyServer {
   }
 
   private async startServer(app: Application): Promise<void> {
+    if (!config.JWT_TOKEN) {
+      throw new Error('JWT_TOKEN must be provided');
+    }
     try {
       const httpServer: http.Server = new http.Server(app);
       const socketIO: Server = await this.createSocketIO(httpServer);
       this.startHttpServer(httpServer);
-      this.scoketIOConnection(socketIO);
+      this.socketIOConnections(socketIO);
     } catch (error) {
       log.error(error);
     }
@@ -101,7 +117,6 @@ export class ChattyServer {
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
       }
     });
-
     const pubClient = createClient({ url: config.REDIS_HOST });
     const subClient = pubClient.duplicate();
     await Promise.all([pubClient.connect(), subClient.connect()]);
@@ -110,26 +125,26 @@ export class ChattyServer {
   }
 
   private startHttpServer(httpServer: http.Server): void {
+    log.info(`Worker with process id of ${process.pid} has started...`);
     log.info(`Server has started with process ${process.pid}`);
-
-    httpServer.listen(SERVER_PORT, '127.0.0.1', () => {
+    httpServer.listen(SERVER_PORT, () => {
       log.info(`Server running on port ${SERVER_PORT}`);
     });
   }
 
-  private scoketIOConnection(io: Server): void {
-    const postSockeHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
-    const followerSockeHandler: SocketIOFollowerHandler = new SocketIOFollowerHandler(io);
-    const userSockeHandler: SocketIOUserHandler = new SocketIOUserHandler(io);
-    const notificationSockeHandler: SocketIONotificationHandler = new SocketIONotificationHandler();
-    const imageSockeHandler: SocketIOImageHandler = new SocketIOImageHandler();
+  private socketIOConnections(io: Server): void {
+    const postSocketHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
+    const followerSocketHandler: SocketIOFollowerHandler = new SocketIOFollowerHandler(io);
+    const userSocketHandler: SocketIOUserHandler = new SocketIOUserHandler(io);
     const chatSocketHandler: SocketIOChatHandler = new SocketIOChatHandler(io);
+    const notificationSocketHandler: SocketIONotificationHandler = new SocketIONotificationHandler();
+    const imageSocketHandler: SocketIOImageHandler = new SocketIOImageHandler();
 
-    postSockeHandler.listen();
-    followerSockeHandler.listen();
-    userSockeHandler.listen();
+    postSocketHandler.listen();
+    followerSocketHandler.listen();
+    userSocketHandler.listen();
     chatSocketHandler.listen();
-    notificationSockeHandler.listen(io);
-    imageSockeHandler.listen(io);
+    notificationSocketHandler.listen(io);
+    imageSocketHandler.listen(io);
   }
 }
