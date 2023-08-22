99ba97c5697a0d6d365dc5459162daae496e3cbf
Peter Trofimov <ptrofimov7@gmail.com>
2023-08-22 10:05:08 +0300

    create react-chatty-app

---
+++ frontend/src/pages/auth/index.js
@@ -0,0 +1,7 @@
+import AuthTabs from '@root/pages/auth/auth-tabs/AuthTabs';
+import ForgotPassword from '@root/pages/auth/forgot-password/ForgotPassword';
+import Login from '@root/pages/auth/login/Login';
+import Register from '@root/pages/auth/register/Register';
+import ResetPassword from '@root/pages/auth/reset-password/ResetPassword';
+
+export { AuthTabs, ForgotPassword, Login, Register, ResetPassword };
